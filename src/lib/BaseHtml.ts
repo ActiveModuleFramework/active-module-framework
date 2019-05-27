d41c334eef8a81b61f00bfdbde33f59c60ba9ebc
SoraKumo <info@croud.jp>
2019-05-27 14:27:05 +0900

    TSLintに基づいてソースを修正

--- src/lib/BaseHtml.ts
+++ src/lib/BaseHtml.ts
@@ -1,138 +1,160 @@
-import * as util from 'util';
-import * as fs from 'fs';
-import * as path from 'path'
-import * as express from 'express'
-import {sprintf} from 'sprintf'
+import * as util from "util";
+import * as fs from "fs";
+import * as path from "path";
+import * as express from "express";
+import { sprintf } from "sprintf";
 
-interface FileInfo{
-	dir: string
-	name: string
-	date: Date
+interface FileInfo {
+  dir: string;
+  name: string;
+  date: Date;
 }
-
 
 /**
  *トップページ表示用クラス
-*
-* @export
-* @class BaseHtml
-*/
+ *
+ * @export
+ * @class BaseHtml
+ */
 export class BaseHtml {
-	/**
-	 *初期ページの出力
-	*
-	* @static
-	* @param {express.Response} res	レスポンス
-	* @param {string[]} cssPath		CSSディレクトリ
-	* @param {string[]} jsPath		JSディレクトリ
-	* @param {string[]} priorityJs	優先度の高いJSファイル
-	* @memberof BaseHtml
-	*/
-	/**
-	 *初期ページの出力
-	 *
-	 * @static
-	 * @param {express.Response} res	レスポンス
-	 * @param {string} baseUrl			基本パス
-	 * @param {string} rootPath			ルートパス
-	 * @param {string} indexPath		index.htmlテンプレートパス
-	 * @param {string[]} cssPath		CSSディレクトリ
-	 * @param {string[]} jsPath			JSディレクトリ
-	 * @param {string[]} priorityJs		優先度の高いJSファイル
-	 * @returns
-	 * @memberof BaseHtml
-	 */
-	static async output(res: express.Response, baseUrl:string,rootPath:string,
-		indexPath:string,cssPath: string[], jsPath: string[], priorityJs: string[]) {
-		function createJSInclude(files: FileInfo[]) {
-			let s = "";
-			for (const file of files) {
-				const dir = (file as any).dir
-				s += util.format('\n\t<script type="text/javascript" src="%s/%s"></script>', dir, file.name);
-			}
-			return s;
-		}
-		function createCSSInclude(files: FileInfo[]) {
-			let s = "";
-			for (const file of files) {
-				const dir = (file as any).dir
-				s += util.format('\n\t<link rel="stylesheet" href="%s/%s">', dir,file.name);
-			}
-			return s;
-		}
-		//ファイル名に日付情報を追加
-		function addDateParam(files: FileInfo[]){
-			for (const file of files) {
-				const date = file.date
-				file.name +=
-					sprintf('?ver=%04d%02d%02d%02d%02d%02d',
-						date.getFullYear(),
-						date.getMonth()+1,
-						date.getDate(),
-						date.getHours(),
-						date.getMinutes(),
-						date.getSeconds())
-			}
-		}
+  /**
+   *初期ページの出力
+   *
+   * @static
+   * @param {express.Response} res	レスポンス
+   * @param {string[]} cssPath		CSSディレクトリ
+   * @param {string[]} jsPath		JSディレクトリ
+   * @param {string[]} priorityJs	優先度の高いJSファイル
+   * @memberof BaseHtml
+   */
+  /**
+   *初期ページの出力
+   *
+   * @static
+   * @param {express.Response} res	レスポンス
+   * @param {string} baseUrl			基本パス
+   * @param {string} rootPath			ルートパス
+   * @param {string} indexPath		index.htmlテンプレートパス
+   * @param {string[]} cssPath		CSSディレクトリ
+   * @param {string[]} jsPath			JSディレクトリ
+   * @param {string[]} priorityJs		優先度の高いJSファイル
+   * @returns
+   * @memberof BaseHtml
+   */
+  public static async output(
+    res: express.Response,
+    baseUrl: string,
+    rootPath: string,
+    indexPath: string,
+    cssPath: string[],
+    jsPath: string[],
+    priorityJs: string[]
+  ): Promise<boolean> {
+    function createJSInclude(files: FileInfo[]): string {
+      let s = "";
+      for (const file of files) {
+        const dir = (file as any).dir;
+        s += util.format(
+          '\n\t<script type="text/javascript" src="%s/%s"></script>',
+          dir,
+          file.name
+        );
+      }
+      return s;
+    }
+    function createCSSInclude(files: FileInfo[]): string {
+      let s = "";
+      for (const file of files) {
+        const dir = (file as any).dir;
+        s += util.format(
+          '\n\t<link rel="stylesheet" href="%s/%s">',
+          dir,
+          file.name
+        );
+      }
+      return s;
+    }
+    //ファイル名に日付情報を追加
+    function addDateParam(files: FileInfo[]): void {
+      for (const file of files) {
+        const date = file.date;
+        file.name += sprintf(
+          "?ver=%04d%02d%02d%02d%02d%02d",
+          date.getFullYear(),
+          date.getMonth() + 1,
+          date.getDate(),
+          date.getHours(),
+          date.getMinutes(),
+          date.getSeconds()
+        );
+      }
+    }
 
-		let html;
-		try{
-			html = fs.readFileSync(indexPath, 'utf-8')
-		}catch(e){
-			return false
-		}
-		const cssFiles: FileInfo[] = []
-		const jsFiles: FileInfo[] = []
-		//CSSファイルリストの読み込み
-		for (let dir of cssPath){
-			try{
-				const files = fs.readdirSync(`${rootPath}/${dir}`)
-				for (const name of files) {
-					if (path.extname(name).toLowerCase() === '.css') {
-						const stat = fs.statSync(`${rootPath}/${dir}/${name}`)
-						cssFiles.push({ dir, name, date: stat.mtime })
-					}
-				}
-			}catch(e){}
+    let html;
+    try {
+      html = fs.readFileSync(indexPath, "utf-8");
+    } catch (e) {
+      return false;
+    }
+    const cssFiles: FileInfo[] = [];
+    const jsFiles: FileInfo[] = [];
+    //CSSファイルリストの読み込み
+    for (let dir of cssPath) {
+      try {
+        const files = fs.readdirSync(`${rootPath}/${dir}`);
+        for (const name of files) {
+          if (path.extname(name).toLowerCase() === ".css") {
+            const stat = fs.statSync(`${rootPath}/${dir}/${name}`);
+            cssFiles.push({ dir, name, date: stat.mtime });
+          }
+        }
+      } catch (e) {
+        // continue
+      }
+    }
+    //JSファイルリストの読み込み
+    for (let dir of jsPath) {
+      try {
+        const files = fs.readdirSync(`${rootPath}/${dir}`);
+        for (const name of files) {
+          if (path.extname(name).toLowerCase() === ".js") {
+            const stat = fs.statSync(`${rootPath}/${dir}/${name}`);
+            jsFiles.push({ dir, name, date: stat.mtime });
+          }
+        }
+      } catch (e) {
+        // continue
+      }
+    }
+    //JSを優先順位に従って並び替え
+    jsFiles.sort((a, b):number => {
+      const v1 = priorityJs.indexOf(a.name);
+      const v2 = priorityJs.indexOf(b.name);
+      return v2 - v1;
+    });
 
-		}
-		//JSファイルリストの読み込み
-		for (let dir of jsPath) {
-			try {
-				const files = fs.readdirSync(`${rootPath}/${dir}`)
-				for (const name of files) {
-					if (path.extname(name).toLowerCase() === '.js') {
-						const stat = fs.statSync(`${rootPath}/${dir}/${name}`)
-						jsFiles.push({ dir, name, date: stat.mtime })
-					}
-				}
-			} catch (e) { }
-		}
-		//JSを優先順位に従って並び替え
-		jsFiles.sort((a,b)=>{
-			const v1 = priorityJs.indexOf(a.name)
-			const v2 = priorityJs.indexOf(b.name)
-			return v2 - v1
-		})
+    //時間情報の追加(キャッシュ対策)
+    addDateParam(jsFiles);
+    addDateParam(cssFiles);
 
-		//時間情報の追加(キャッシュ対策)
-		addDateParam(jsFiles)
-		addDateParam(cssFiles)
+    const data = html
+      .replace("[[SCRIPTS]]", createJSInclude(jsFiles))
+      .replace("[[CSS]]", createCSSInclude(cssFiles));
+    const links: string[] = [];
+    for (const file of cssFiles) {
+      const dir = (file as any).dir;
+      links.push(`<${baseUrl}${dir}/${file.name}>;rel=preload;as=style;`);
+    }
+    for (const file of jsFiles) {
+      const dir = (file as any).dir;
+      links.push(`<${baseUrl}${dir}/${file.name}>;rel=preload;as=script;`);
+    }
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=UTF-8",
+      link: links
+    });
+    res.end(data);
 
-		const data = html.replace("[[SCRIPTS]]", createJSInclude(jsFiles))
-			.replace("[[CSS]]", createCSSInclude(cssFiles))
-		const links:string[] = []
-		for(const file of cssFiles){
-			const dir = (file as any).dir
-			links.push(`<${baseUrl}${dir}/${file.name}>;rel=preload;as=style;`)
-		}
-		for (const file of jsFiles) {
-			const dir = (file as any).dir
-			links.push(`<${baseUrl}${dir}/${file.name}>;rel=preload;as=script;`)
-		}
-		res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8','link':links });
-		res.end(data)
-
-		return true
-	}
+    return true;
+  }
 }
--- src/lib/LocalDB.ts
+++ src/lib/LocalDB.ts
@@ -1,5 +1,5 @@
-import * as uuid from 'uuid/v4'
-import { SQLiteDB } from './SQLiteDB';
+import * as uuid from "uuid/v4";
+import { SQLiteDB } from "./SQLiteDB";
 
 /**
  *ローカルDB制御用クラス
@@ -8,66 +8,89 @@ import { SQLiteDB } from './SQLiteDB';
  * @class LocalDB
  * @extends {SQLiteDB}
  */
-export class LocalDB extends SQLiteDB{
-	/**
-	 *セッション用DBの初期化
-	 *
-	 * @memberof LocalDB
-	 */
-	async initDB(){
-		await this.run('CREATE TABLE IF NOT EXISTS session (id text primary key,date real,server json)')
-		await this.run('CREATE INDEX IF NOT EXISTS idx_session_date on session(date)')
-	}
+export class LocalDB extends SQLiteDB {
+  /**
+   *セッション用DBの初期化
+   *
+   * @memberof LocalDB
+   */
+  public async initDB(): Promise<void> {
+    await this.run(
+      "CREATE TABLE IF NOT EXISTS session (id text primary key,date real,server json)"
+    );
+    await this.run(
+      "CREATE INDEX IF NOT EXISTS idx_session_date on session(date)"
+    );
+  }
 
-
-	/**
-	 *
-	 *
-	 * @param {string} hash
-	 * @param {number} expire
-	 * @returns {Promise<{ hash: string, values: { [key: string]: any }}>}
-	 * @memberof LocalDB
-	 */
-	public async startSession(hash: string, expire: number): Promise<{ hash: string, values: { [key: string]: any }}>{
-		let id = hash;
-		if (id){
-			//一時間経過したセッションを削除
-			await this.run("delete from session where date < datetime(current_timestamp , ?||' hour')", -expire)
-			//セッションを抽出
-			let result = await this.get("select id,server from session where id=?",id)
-			if (result) {
-				return { hash: result.id, values: JSON.parse(result.server)}
-			}
-		}
-		return {hash:await this.createSession(),values:{}}
-	}
-	/**
-	 *
-	 *
-	 * @param {string} hash
-	 * @param {{[key:string]:any}} values
-	 * @returns
-	 * @memberof LocalDB
-	 */
-	public async endSession(hash:string,values:{[key:string]:any}){
-		this.run("update session set date=current_timestamp,server=? where id=?",
-			JSON.stringify(values), hash)
-		return true;
-	}
-	/**
-	 *
-	 *
-	 * @returns
-	 * @memberof LocalDB
-	 */
-	public async createSession(){
-		var id:string|null = uuid();
-		do{
-			const result = await this.all("select id from session where id=?",id)
-			if(result.length)
-				id = null
-		}while(id === null);
-		await this.run("insert into session values(?,CURRENT_TIMESTAMP,json_object())",id)
-		return id;
-	}
+  /**
+   *
+   *
+   * @param {string} hash
+   * @param {number} expire
+   * @returns {Promise<{ hash: string, values: { [key: string]: any }}>}
+   * @memberof LocalDB
+   */
+  public async startSession(
+    hash: string,
+    expire: number
+  ): Promise<{ hash: string; values: { [key: string]: any } }> {
+    let id = hash;
+    if (id) {
+      //一時間経過したセッションを削除
+      await this.run(
+        "delete from session where date < datetime(current_timestamp , ?||' hour')",
+        -expire
+      );
+      //セッションを抽出
+      let result = await this.get(
+        "select id,server from session where id=?",
+        id
+      );
+      if (result) {
+        return {
+          hash: result.id as string,
+          values: JSON.parse(result.server as string)
+        };
+      }
+    }
+    return { hash: await this.createSession(), values: {} };
+  }
+  /**
+   *
+   *
+   * @param {string} hash
+   * @param {{[key:string]:any}} values
+   * @returns
+   * @memberof LocalDB
+   */
+  public async endSession(
+    hash: string,
+    values: { [key: string]: unknown }
+  ): Promise<boolean> {
+    this.run(
+      "update session set date=current_timestamp,server=? where id=?",
+      JSON.stringify(values),
+      hash
+    );
+    return true;
+  }
+  /**
+   *
+   *
+   * @returns
+   * @memberof LocalDB
+   */
+  public async createSession(): Promise<string> {
+    var id: string | null = uuid();
+    do {
+      const result = await this.all("select id from session where id=?", id);
+      if (result.length) id = null;
+    } while (id === null);
+    await this.run(
+      "insert into session values(?,CURRENT_TIMESTAMP,json_object())",
+      id
+    );
+    return id;
+  }
 }
