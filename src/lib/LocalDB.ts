7c39ea2d2d4bd52c7f40b42e607bc1c10ead2c17
SoraKumo <info@croud.jp>
2019-07-03 11:14:30 +0900

    大幅修正

--- src/lib/LocalDB.ts
+++ src/lib/LocalDB.ts
@@ -18,7 +18,7 @@ export class LocalDB extends SQLiteDB {
    */
   public async initDB(): Promise<void> {
     await this.run(
-      "CREATE TABLE IF NOT EXISTS session (id text primary key,date real,server json)"
+      "CREATE TABLE IF NOT EXISTS session (id text primary key,type text,date real,server json)"
     );
     await this.run(
       "CREATE INDEX IF NOT EXISTS idx_session_date on session(date)"
@@ -34,14 +34,14 @@ export class LocalDB extends SQLiteDB {
   }
 
   /**
-   *
+   *セッションの開始と関連データの更新
    *
    * @param {string} hash
    * @param {number} expire
    * @returns {Promise<{ hash: string, values: { [key: string]: any }}>}
    * @memberof LocalDB
    */
-  public async startSession(
+  public async startSession(type:string,
     hash: string | null,
     expire: number
   ): Promise<{ hash: string; values: { [key: string]: any } }> {
@@ -49,13 +49,13 @@ export class LocalDB extends SQLiteDB {
     if (id) {
       //一時間経過したセッションを削除
       await this.run(
-        "delete from session where date < datetime(current_timestamp , ?||' hour')",
-        -expire
+        "delete from session where date < datetime(current_timestamp , ?||' second') and type=?",
+        -expire,type
       );
       //セッションを抽出
       let result = await this.get(
-        "select id,server from session where id=?",
-        id
+        "select id,server from session where id=? and type=?",
+        id,type
       );
 
       if (result) {
@@ -67,10 +67,10 @@ export class LocalDB extends SQLiteDB {
         };
       }
     }
-    return { hash: await this.createSession(), values: {} };
+    return { hash: await this.createSession(type), values: {} };
   }
   /**
-   *
+   *セッションの終了とデータの保存
    *
    * @param {string} hash
    * @param {{[key:string]:any}} values
@@ -89,25 +89,32 @@ export class LocalDB extends SQLiteDB {
     return true;
   }
   /**
-   *
+   *セッションの作成
    *
    * @returns
    * @memberof LocalDB
    */
-  public async createSession(): Promise<string> {
+  /**
+   *セッションの作成
+   *
+   * @param {string} type セッションタイプ GLOBAL | SESSION
+   * @returns {Promise<string>}
+   * @memberof LocalDB
+   */
+  public async createSession(type:string): Promise<string> {
     var id: string | null = uuid();
     do {
-      const result = await this.all("select id from session where id=?", id);
+      const result = await this.all("select id from session where id=? and type=?", id,type);
       if (result.length) id = null;
     } while (id === null);
     await this.run(
-      "insert into session values(?,CURRENT_TIMESTAMP,json_object())",
-      id
+      "insert into session values(?,?,CURRENT_TIMESTAMP,json_object())",
+      id,type
     );
     return id;
   }
   /**
-   *
+   *セッション用データの設定
    *
    * @param {string} name
    * @param {*} value
@@ -135,13 +142,8 @@ export class LocalDB extends SQLiteDB {
       }
     }
   }
-  /*
-  public setItem(value:{[key:string]:unknown}): void {
-    this.items[name] = value;
-    this.run("replace into app_data values(?,?)", name, JSON.stringify(value));
-  }*/
   /**
-   *
+   *セッション用データの取得
    *
    * @param {string} name
    * @returns {*}
